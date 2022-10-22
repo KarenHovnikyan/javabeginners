100e04e31858ef9718a4a12e043a5b7d01044d96
Karen <hovnikyank@gmail.com>
2022-10-22 04:43:59 +0400

    add Methods

---
+++ src/com/company/methods/Methods.java
@@ -0,0 +1,44 @@
+package com.company.methods;
+
+
+/**
+ * @author Karen
+ */
+public class Methods {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Write a method which  .has two input parameters  type  int and return 0.
+     *
+     * @param a
+     * @param b
+     * @return a
+     */
+    int return0(int a, int b) {
+        return 0;
+    }
+
+    /**
+     * Write a method which  .has  two float input parameters  and return  one of them.
+     *
+     * @param a
+     * @param b
+     * @return a
+     */
+    float floatOneParametrs(float a, float b) {
+        return a;
+    }
+
+    /**
+     * Write a method which  .has  two float input parameters  and return  one of them.
+     *
+     * @return a
+     */
+    void recursion() {
+        recursion();
+    }
+
+}
