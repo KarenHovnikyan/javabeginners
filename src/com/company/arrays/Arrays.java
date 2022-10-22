8e59c7b0bb27718f511d4aee4df0f610daa78045
Karen <hovnikyank@gmail.com>
2022-10-22 07:13:24 +0400

    add Arrays class

---
+++ src/com/company/arrays/Arrays.java
@@ -0,0 +1,19 @@
+package com.company.arrays;
+
+/**
+ * @author Karen
+ */
+public class Arrays {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Write a method that checks whether the second digit of a given three-digit number is 7 or not.
+     * For example. 974 -> true, 846 -> false
+     *
+     * @param n
+     * @return true or false
+     */
+
+}
