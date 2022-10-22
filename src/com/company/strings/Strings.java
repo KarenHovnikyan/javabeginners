c1fb05d93e25c72de13d3df21f8d727bbd302989
Karen <hovnikyank@gmail.com>
2022-10-23 03:27:34 +0400

    add String methods

--- src/com/company/strings/Strings.java
+++ src/com/company/strings/Strings.java
@@ -13,6 +13,25 @@ public class Strings {
 
         String result = st.strRevers("abcde");
         System.out.println(result);
+
+
+        int count = st.countSubString("iw w w wi", "w");
+        System.out.println(count);
+
+        StringBuilder sts = new StringBuilder("avs");
+        System.out.println(sts.toString().toUpperCase());
+
+        st.print20Symbols("Kinomateria");
+
+        System.out.println("karin benzeman".replace('a', ' '));
+
+        String str = st.removeA("ameno ameno ameno amenoa", 'a');
+        System.out.println(str);
+
+        System.out.println("minasian nilta".substring(0, 7));
+
+        st.isEmptyStr("");
+
     }
 
     /**
@@ -52,12 +71,63 @@ public class Strings {
      * @return count
      */
     int countSubString(String line, String token) {
-        int count = 0;
-        for (int i = 0; i < line.length(); i++) {
-
+        String[] str = line.split(token);
+        int count = str.length;
+        if (line.endsWith(token)) {
+            count++;
         }
+        return count - 1;
+    }
 
-        return count;
+    /**
+     * Write a method that reads a line of 20 characters from the console, prints it,
+     * if the length of the line is less than 20,then we must
+     * silence the line by writing the symbol "*" instead of less characters.
+     *
+     * @param str
+     */
+    void print20Symbols(String str) {
+        System.out.println(str);
+        while (str.length() < 20) {
+            str += "*";
+        }
+        System.out.println(str);
+    }
+
+    /**
+     * Write a method that removes all "a" characters from the given text.
+     *
+     * @param str
+     * @param a
+     * @return newStr
+     */
+    String removeA(String str, char a) {
+        String newStr = new String();
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] != a) {
+                newStr += ch[i];
+            }
+        }
+        return newStr;
+    }
+
+
+    /**
+     * Write a method that checks if the given String parameter is empty and not null:
+     *
+     * @param str
+     */
+
+    void isEmptyStr(String str) {
+        if (str.isEmpty()) {
+            System.out.println("str isEmpty");
+        }
+        if (str == null) {
+            System.out.println("str is a null");
+        } else {
+            System.out.println("str is a not null and not Empty");
+        }
     }
 
 }
