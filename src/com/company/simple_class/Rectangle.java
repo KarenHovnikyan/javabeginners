a26ad75ea1cdcdc9f5c4e6d21e672ae4e2082484
Karen <hovnikyank@gmail.com>
2022-10-23 04:22:33 +0400

    remove main in Rectange class

--- src/com/company/simple_class/Rectangle.java
+++ src/com/company/simple_class/Rectangle.java
@@ -7,14 +7,6 @@ public class Rectangle {
     public int length;
     public int width;
 
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
-        rectangle.length = 5;
-        rectangle.width = 14;
-        int result = rectangle.calculateArea();
-        System.out.println(result);
-    }
-
     /**
      * Multiplicate length and width
      *
