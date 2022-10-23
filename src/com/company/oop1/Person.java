c08fe6888611e58d593360530d69db68b0248b41
Karen <hovnikyank@gmail.com>
2022-10-23 05:09:13 +0400

    add class Person

---
+++ src/com/company/oop1/Person.java
@@ -0,0 +1,27 @@
+package com.company.oop1;
+
+/**
+ * @author Karen
+ */
+public class Person {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        if (newAge > 0) {
+            age = newAge;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
