e78f894c445680ddb1cc1372d28657db9aa82635
Karen <hovnikyank@gmail.com>
2022-10-23 05:09:27 +0400

    add class Test

---
+++ src/com/company/oop1/Test.java
@@ -0,0 +1,18 @@
+package com.company.oop1;
+
+/**
+ * @author Karen
+ */
+public class Test {
+    public static void main(String args[]) {
+        Person person = new Person();
+        //person.name = "Hanna";
+        //person.age = -8;//DOES NOT COMPILE
+        person.setName("Lolo");
+        person.setAge(10);
+
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
+
+    }
+}
