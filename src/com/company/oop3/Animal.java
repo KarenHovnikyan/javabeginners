87472ee18b0ae276ccfa0ec68073dd1f844df8bc
Karen <hovnikyank@gmail.com>
2022-10-23 22:29:29 +0400

    add abstract interface Animale

---
+++ src/com/company/oop3/Animal.java
@@ -0,0 +1,20 @@
+package com.company.oop3;
+
+/**
+ * @author Karen
+ */
+public abstract interface Animal {
+
+    public abstract void animalSound();
+
+}
+
+
+class Pig implements Animal {
+
+    @Override
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+
+}
