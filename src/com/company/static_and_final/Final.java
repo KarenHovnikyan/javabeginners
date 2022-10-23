4e06016de9b6213218e49fe0791696fe485e254e
Karen <hovnikyank@gmail.com>
2022-10-23 04:44:24 +0400

    add Finale class final variables final methods

--- src/com/company/static_and_final/Final.java
+++ src/com/company/static_and_final/Final.java
@@ -4,4 +4,33 @@ package com.company.static_and_final;
  * @author Karen
  */
 public class Final {
+    final int m = 77;
+    final int k;
+    final int v;
+
+    {
+        k = 10; // Initialize in instance block
+    }
+
+    public Final(int a) {
+        v = a; // Initialize in constructor block
+    }
+
+    public void add(final char c) {
+        final int i = 80;
+
+        //  i = 10;   //DOES NOT COMPILE
+        // k = 10;   //DOES NOT COMPILE
+        //  c = ‘f’;  //DOES NOT COMPILE
+    }
+
+    public void add() {
+        final int i;// Initialize for locals is anywhere inside method
+        i = 10;
+        //  i = 11;//DOES NOT COMPILE
+
+        //  k = 10;//DOES NOT COMPILE
+    }
+
+
 }
