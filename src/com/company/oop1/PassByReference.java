498116b815f3e08bb0bfdc628ff3f20f0ce3c6fb
Karen <hovnikyank@gmail.com>
2022-10-24 21:54:43 +0400

    change PassByReference and PassByValue classis

--- src/com/company/oop1/PassByReference.java
+++ src/com/company/oop1/PassByReference.java
@@ -4,6 +4,9 @@ package com.company.oop1;
  * @author Karen
  */
 public class PassByReference {
+    public static void main(String[] args) {
+
+    }
 
     public void print(StringBuilder n) {
         n.append("World");
--- src/com/company/oop1/PassByValue.java
+++ src/com/company/oop1/PassByValue.java
@@ -4,7 +4,9 @@ package com.company.oop1;
  * @author Karen
  */
 public class PassByValue {
+    public static void main(String[] args) {
 
+    }
 
     public void print(int i) {
         i += 5;
