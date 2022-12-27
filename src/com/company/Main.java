e61c146e7bbbfa3bbbb1e33c457e2058c891eb72
KarenHovnikyan <hovnikyank@gmail.com>
2022-12-27 18:59:56 +0400

    add 3 System out printl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hello new worlds");
+        System.out.println("hello new worlds");
+        System.out.println("hello new worlds");
+
+
+
     }
 }
 
